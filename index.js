5b9fe1b029a8662b3977da5e3e79824f5b7b0700
Duke Ko <dukhyunko93@gmail.com>
2020-05-15 15:49:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,21 +18,12 @@ function sayHiToGrandma(string) {
   if (string.toLowercase() === string) {
     return "I can't hear you!"
   }
+
   if (string.toUppdercase() === string) {
     return "YES INDEED!"
   }
+
   if (string === "I love you, Grandma") {
     return "I love you, too."
   }
 }
-
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  }
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
